b163ea6367613bca7da37ffcbebf41f56dfb12ba
egrizq <rizq.ramadhan17@gmail.com>
2024-06-12 07:08:01 +0700

    feat: components experience

---
+++ src/components/Experience.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+    <div class="flex flex-col pt-5">
+        <p class="pb-2 text-lg">Experience</p>
+        
+        <div class="flex flex-row space-x-1">
+            <div class="text-zinc-600 w-full">
+                <ul class="ml-5">
+                    <li class="list-disc">
+                        <div class="flex flex-row justify-between">
+                            <p class="text-base text-black">Calibration Engineering</p>
+                            <p>2021-2023</p>
+                        </div>
+                        <p>PT. Suryaraya Rubberindo Industries</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
