cdf2b6194ea37b04cbd21bad3b1e8ca1a8c5cd53
egrizq <rizq.ramadhan17@gmail.com>
2024-07-01 07:08:33 +0700

    feat: journal w medium

---
+++ src/Journal.vue
@@ -0,0 +1,47 @@
+<script setup>
+    import { reactive } from 'vue';
+    
+    const listJournal = reactive([  
+        {
+            title: "How Programming Shapes your  Mind!",
+            link: "https://medium.com/@rizq.ramadhan17/how-programming-shapes-your-mind-57ce82ebb808",
+            date: "Jul 01, 2024"
+        },
+        {
+            title: "Apa itu kalibrasi?",
+            link: "https://medium.com/@rizq.ramadhan17/apa-itu-kalibrasi-81020acc5e18",
+            date: "Dec 11, 2023"
+        },
+        {
+            title: "My Reflection on Data Cleaning",
+            link: "https://medium.com/@rizq.ramadhan17/my-reflection-on-data-cleaning-426cf592abb9",
+            date: "Oct 18, 2023"
+        },
+        {
+            title: "Are You Fear?",
+            link: "https://medium.com/@rizq.ramadhan17/are-you-fear-f1449f2f129a",
+            date: "Jul 08, 2023"
+        },
+    ])
+
+</script>
+
+<template>
+     <main class="ml-2"
+        data-aos="fade-up"
+        data-aos-anchor-placement="top-bottom"
+        data-aos-duration="800">
+        
+        <div class="space-y-5 text-sm">
+            <div v-for="(data, index) in listJournal" :key="index" >
+                <div class="flex border-b pb-2 justify-between">
+                        <a class="hover:text-blue-500" 
+                        :href="data.link">
+                        {{ data.title }}
+                    </a>
+                    <p class="text-zinc-600 italic">{{ data.date }}</p>                
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
